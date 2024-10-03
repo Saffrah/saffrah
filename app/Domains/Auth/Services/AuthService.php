0d60a9c05f0838fa6aa31e7253e56c8d28a163c9
Mohab El-Sayed <mohab.elsayed@cassbana.com>
2024-10-03 23:01:37 +0300

    make auth module login and register for both usrers

--- app/Domains/Auth/Services/AuthService.php
+++ app/Domains/Auth/Services/AuthService.php
@@ -2,21 +2,40 @@
 
 namespace App\Domains\Auth\Services;
 
+// Repositories
 use App\Domains\Auth\Repositories\AuthRepository;
+use App\Domains\Company\Repositories\CompanyRepository;
+
+// Models
+use App\Models\Company;
+use App\Models\User;
+// Libraries
+use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthService
 {
     private $auth_repository;
+    private $company_repository;
 
-    public function __construct(AuthRepository $auth_repository ) 
+    public function __construct(AuthRepository $auth_repository, CompanyRepository $company_repository) 
     {
         $this->auth_repository = $auth_repository;
+        $this->company_repository = $company_repository;
     }
 
     public function register($request) 
     {
-        $result = $this->auth_repository->register($request);
+        if($request['user_type'] == 'user') {
+            $result = $this->auth_repository->register($request);
+            $result['token'] = $result->createToken('User', ['role:user'])->plainTextToken; 
+
+        }
+        else {
+            $result = $this->company_repository->register($request);
+            $result['token'] = $result->createToken('Company', ['role:company'])->plainTextToken; 
+        }
 
         if($result) {
             return [
@@ -29,17 +48,48 @@ class AuthService
         return [
             'response_code'    => 400,
             'response_message' => 'registered failed !', 
-            'response_data'    => []
+            'response_data'    => NULL
         ];
     }
 
     function login($request) 
     {
-        if(Auth::attempt(['email' => $request['email'], 'password' => $request['password']])) { 
+        $result  = false;
+        $message = 'LogIn Credentials are not correct !';
+
+        $user = User::where('email', $request['email'])->first();
+
+        if($user) { 
             /** @var \App\Models\User $user **/
-            $user = auth('sanctum')->user(); 
-            $user['token'] =  $user->createToken('User', ['role:user'])->plainTextToken; 
-   
+            $user              = auth('sanctum')->user(); 
+            if(Hash::check($request['password'], $user->password) && $user->email_verified_at != NULL) {
+                $user['token']     =  $user->createToken('User', ['role:user'])->plainTextToken; 
+                $user['user_type'] = 'User';
+                $result = true;
+            }
+            elseif(Hash::check($request['password'], $user->password) && $user->email_verified_at == NULL) {
+                $message = 'You are not verified yet, please contact the administration !';
+                $user->tokens()->delete();
+            }
+            
+        }
+
+
+        $user = Company::where('email', $request['email'])->first();
+        if($user) {
+            if (Hash::check($request['password'], $user->password) && $user->email_verified_at != NULL) {
+                $user['token']     =  $user->createToken('Company', ['role:company'])->plainTextToken; 
+                $user['user_type'] = 'Company';
+                $result = true;
+            }
+            elseif(Hash::check($request['password'], $user->password) && $user->email_verified_at == NULL) {
+                $message = 'You are not verified yet, please contact the administration !';
+                $user->tokens()->delete();
+            }
+
+        }
+
+        if($result) {
             return [
                 'response_code'    => 200,
                 'response_message' => 'Logged In Successfully',
@@ -49,8 +99,8 @@ class AuthService
 
         return [
             'response_code'    => 400,
-            'response_message' => 'LogIn Credentials are not correct !',
-            'response_data'    => []
+            'response_message' => $message,
+            'response_data'    => NULL
         ];     
     }
 
