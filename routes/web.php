e29420a09e0b8cdb885edc436183dbcb426c0936
Mohab El-Sayed <mohab.elsayed@cassbana.com>
2024-12-07 11:33:17 +0900

    add route /

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use App\Http\Controllers\AdminController;
 use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return redirect()->route('admin.get.login');
+});
+
 Route::get('/login', [AdminController::class, 'getlogin'])->name('admin.get.login');
 Route::post('/login', [AdminController::class, 'postlogin'])->name('admin.post.login');
 Route::get('/register', [AdminController::class, 'getRegister'])->name('admin.get.register');
