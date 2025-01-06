9be547f45ca65761b8a045c87761cfce782f9571
Mohab El-Sayed <mohab.elsayed@cassbana.com>
2025-01-06 22:15:27 +0900

    add view company

--- resources/views/pages/packages.blade.php
+++ resources/views/pages/packages.blade.php
@@ -257,7 +257,7 @@
                                     <span class="text-secondary text-sm font-weight-normal">{{ $package->end_date ? date("Y-m-d", strtotime($package->end_date)) : "" }}</span>
                                 </td>
                                 <td class="align-middle text-center">
-                                    <a href="javascript:;" class="text-secondary font-weight-bold text-xs m-2 delete cursor-pointer" data-bs-toggle="tooltip" data-bs-title="Delete user">
+                                    <a href="javascript:;" class="text-secondary font-weight-bold text-xs m-2 delete cursor-pointer" data-bs-toggle="tooltip" data-bs-title="Delete Package">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                             <polyline points="3 6 5 6 21 6"></polyline>
                                             <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
