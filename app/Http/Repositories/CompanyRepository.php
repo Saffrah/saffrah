c347e39954e38448bf77572b5989d4f7807f2691
Mohab El-Sayed <mohab.elsayed@cassbana.com>
2025-01-11 17:29:41 +0900

    update company view page

--- app/Http/Repositories/CompanyRepository.php
+++ app/Http/Repositories/CompanyRepository.php
@@ -3,15 +3,18 @@
 namespace App\Http\Repositories;
 
 use App\Models\Company;
+use App\Models\PackageConfirm;
 use Illuminate\Support\Facades\DB;
 
 class CompanyRepository
 {
     private $model;
+    private $package_confirm_model;
 
-    public function __construct(Company $company = null) 
+    public function __construct(Company $company = null, PackageConfirm $package_confirm = null) 
     {
         $this->model = $company;
+        $this->package_confirm_model = $package_confirm;
     }
 
     public function find($id) 
@@ -19,6 +22,15 @@ class CompanyRepository
         return $this->model->with(['packages.confirms' => function ($query) {
             $query->select('package_id', DB::raw('SUM(no_of_guests) as total_guests'))->groupBy('package_id');
         }])->find($id);    
+    }
+
+    public function confirms($company_id) 
+    {
+        return $this->package_confirm_model->join('packages', 'packages.id', 'package_confirms.package_id')
+                                           ->join('companies', 'companies.id', 'packages.company_id')
+                                           ->select('package_confirms.*')
+                                           ->where('companies.id', $company_id)
+                                           ->get();
     }
     
     public function all() 
